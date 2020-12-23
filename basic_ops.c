fee8526c4b771ad7e0dabc6efeac933273bc0131
Dc-cpu-arch <d.garzong77@gmail.com>
2020-12-23 04:09:28 +0000

    nothing on particular

---
+++ basic_ops.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop_cmd - nothing on particular
+ * @head: head of a DLL
+ * @line_number: line of the file
+ */
+void nop_cmd(stack_t **head, unsigned int line_number)
+{
+	(void) head;
+	(void) line_number;
+}
